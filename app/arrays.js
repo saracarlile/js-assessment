df59d2af1d7a76bc55eeb59e041190b1639da6a1
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-08-23 14:40:40 -0500

    array tests

--- app/arrays.js
+++ app/arrays.js
@@ -46,31 +46,54 @@ exports.arraysAnswers = {
   },
 
   prepend: function(arr, item) {
-
+    arr.splice(0, 0, item);
+    return arr;
   },
 
   curtail: function(arr) {
-
+    arr.splice(0, 1);
+    return arr;
   },
 
   concat: function(arr1, arr2) {
-
+    var temp = arr1.concat(arr2);
+    return temp;
   },
 
   insert: function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count: function(arr, item) {
-
+    var count = 0;
+    for(var i=0; i < arr.length; i++){
+      if (arr[i] === item)
+        count += 1;
+    }
+    return count;
   },
 
-  duplicates: function(arr) {
-
+  duplicates: function (arr) {
+    var temp = [];
+    var result = [];
+    for (var i = arr.length; i--;) {
+      var c = arr.shift();
+      if (!temp.includes(c)) {
+        temp.push(c);
+      }
+      else if (!result.includes(c)){
+        result.push(c);
+      }
+    }
+    return result;
   },
 
   square: function(arr) {
-
+    var s = arr.map(function(num) {
+    return num * num;
+  });
+  return s;
   },
 
   findAllOccurrences: function(arr, target) {
