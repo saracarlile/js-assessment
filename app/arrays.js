9136fb4a2e30bfb10c3124059612e3a2f3d19e61
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-08-23 12:42:35 -0500

    arrays test

--- app/arrays.js
+++ app/arrays.js
@@ -1,28 +1,48 @@
+//https://github.com/rmurphey/js-assessment
+
 exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
-
+      return arr.indexOf(item);
   },
 
   sum: function(arr) {
-
+    var l = 0;
+    var count = 0;
+    while (l < arr.length){
+      count = count + arr[l];
+      l += 1;
+    }
+    return count;
   },
 
   remove: function(arr, item) {
-
+    for(var i= arr.length; i--;){
+    if(arr[i] === item){
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
   },
 
   removeWithoutCopy: function(arr, item) {
-
+      for(var i= arr.length; i--;){
+    if(arr[i] === item){
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
   },
 
   append: function(arr, item) {
-
+      arr.push(item);
+      return arr;
   },
 
   truncate: function(arr) {
-
+      arr.pop();
+      return arr;
   },
 
   prepend: function(arr, item) {
