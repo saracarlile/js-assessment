dfd8285fba2c97c18b6a2a916c12bc620db505a6
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-08-26 17:07:30 -0500

    regex answers

--- app/regex.js
+++ app/regex.js
@@ -1,27 +1,35 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
-  containsNumber: function(str) {
-
+  containsNumber: function (str) {
+    var regEx = /[\d+]/g;
+    return regEx.test(str);
   },
 
-  containsRepeatingLetter: function(str) {
-
+  containsRepeatingLetter: function (str) {
+    var regEx = /([A-Za-z])\1/;
+    return regEx.test(str);
   },
 
-  endsWithVowel: function(str) {
-
+  endsWithVowel: function (str) {
+    var regEx = /(a|e|i|o|u|A|E|I|O|U)$/;
+    return regEx.test(str);
   },
 
-  captureThreeNumbers: function(str) {
-
+  captureThreeNumbers: function (str) {
+     var regEx = /\d{3}/;
+     var matches = str.match(regEx);
+    return matches ? matches[0] : false;
   },
 
-  matchesPattern: function(str) {
-
+  matchesPattern: function (str) {
+    var regEx = /^(\d{3})-(\d{3})-(\d{4})$/;
+    return regEx.test(str);
   },
 
-  isUSD: function(str) {
-
+  isUSD: function (str) {
+    var regEx = /^\$\d{1,3}(,\d{3})*(\.\d{2})?$/;
+    return regEx.test(str);
   }
 };
+ 
